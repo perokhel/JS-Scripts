d889a925f6cf314fe7ac0f339d73f3e72f8d4e8d
Hamid Ali <user@email.com>
2024-06-27 15:09:30 +1000

    Updated coles website script to make it more modular and separate web scrapping functions from http requests etc.

--- colescardsummary.mjs
+++ colescardsummary.mjs
@@ -15,18 +15,7 @@ function getColesFinancialSummary() {
     ? extractNumber(installmentBalanceField.innerText)
     : 0;
 
-  const dataRows = Array.from(
-    document.querySelectorAll(".custom-data-grid-row-identifier")
-  );
-
-  const pendingTransactions = dataRows.map((row) => {
-    const rowCells = row.querySelectorAll(".cbol-grid-cell");
-    const transactionObj = {};
-    transactionObj.date = rowCells[0].innerText;
-    transactionObj.description = rowCells[1].innerText;
-    transactionObj.amount = extractNumber(rowCells[2].innerText);
-    return transactionObj;
-  });
+  const pendingTransactions = getPendingTransactions();
 
   const pendingTransactionsTotal = pendingTransactions.reduce(
     (sum, transaction) => sum + transaction.amount,
@@ -47,6 +36,65 @@ function getColesFinancialSummary() {
     pendingTransactionsTotal,
     reconciliationTotal,
   };
+}
+
+function getPendingTransactions() {
+  const dataRows = Array.from(
+    document.querySelectorAll(".custom-data-grid-row-identifier")
+  );
+
+  return dataRows.map((row) => {
+    const rowCells = row.querySelectorAll(".cbol-grid-cell");
+    const transactionObj = {};
+    transactionObj.date = rowCells[0].innerText;
+    transactionObj.description = rowCells[1].innerText;
+    transactionObj.amount = extractNumber(rowCells[2].innerText);
+    return transactionObj;
+  });
+}
+
+function getTransactions() {
+  let transactions, transactionsArray, transMap;
+  transactions = document.querySelectorAll(".custom-data-grid-row-identifier");
+  transactionsArray = Array.from(transactions);
+  transMap = transactionsArray.map((trans) => {
+    let dataPivot = trans.attributes["data-pivot"].value;
+    let date = trans.querySelector(".ui-float-left").innerText;
+    let description = trans.querySelector(".cbol-trans-desc").innerText;
+    let amount = trans.querySelector(".format-amount-holder").innerText;
+    return { dataPivot, date, description, amount };
+  });
+
+  return transMap;
+}
+
+async function sendData(transactions) {
+  let apiUrl = "http://localhost:3100/api";
+  let apiUrlCyclic = "https://gardenia.cyclic.app/api";
+  let postOptions = {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    headers: {
+      "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(transactions), // body data type must match "Content-Type" header
+  };
+  let res = await fetch(apiUrl, postOptions);
+  let jsonData = await res.json();
+  console.log(jsonData);
+}
+
+function getTransactionsTotal(transactions) {
+  return transactions.reduce(
+    (sum, transaction) =>
+      extractNumber(transaction.amount) < 0
+        ? sum + extractNumber(transaction.amount)
+        : sum,
+    0
+  );
 }
 
 function extractNumber(str) {
